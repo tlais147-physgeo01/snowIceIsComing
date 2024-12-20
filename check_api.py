edbf7bf5018210c3102a6e96338240d6469246ba
KMicha <michael.kahle@yahoo.de>
2024-12-20 16:33:31 +0100

    check user

--- check_api.py
+++ check_api.py
@@ -43,7 +43,7 @@ def checkGithubOrganization():
       print(test2)
     else:
       print('Running locally?') 
-    htmlReq = requests.get('https://github.com/'+gitOrg+)
+    htmlReq = requests.get('https://github.com/'+gitOrg)
     if(htmlReq.text):
       print(htmlReq.text)
     else:
