9be4acfe2b1a8d17d0fc89e2ef14f877cd210707
KMicha <michael.kahle@yahoo.de>
2024-12-21 11:20:54 +0100

    check for assignment

--- check_api.py
+++ check_api.py
@@ -14,16 +14,16 @@ def inqUrl(url):
    return jsonData
 
 
-def checkNewsApi():
-    results = []
+def checkNewsApi(results=[]):
     apiKey = os.getenv('NEWSAPI_KEY')
+    results.append("### NewsAPI")
     if(apiKey == '1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7'): 
-        results.append("### NewsAPI")
+        results.append("NewsAPI missing:")
         results.append("1. Please register at https://newsapi.org/register")
         results.append("2. Login and get your API key at https://newsapi.org/account")
         results.append("3. Assign the API key as new organization secret at https://github.com/organizations/newsWhisperer/settings/secrets/actions/new")       
-        results.append("   Name:  NEWSAPI_KEY ")
-        results.append("   Value: <Your key here> ")   
+        results.append("   * Name:  NEWSAPI_KEY ")
+        results.append("   * Value: <Your key here> ")   
     return results
 
 
@@ -31,27 +31,44 @@ def checkNewsApi():
 #  'https://api.github.com/users/'+gitOrg   "type": "Organization" / "type": "User" ??
 # https://docs.github.com/en/rest/orgs/members?apiVersion=2022-11-28
 #  'https://api.github.com/orgs/ORG/members'    -> type,url
+# https://stackoverflow.com/questions/31338803/only-few-of-my-github-organizations-are-shown-in-my-public-github-profile
 
-def checkGithubOrganization():
+def checkGithubOrganization(results=[]):
     gitOrg = os.getenv('GITHUB_OWNER')
     gitRepo = os.getenv('GITHUB_REPO')
     print(['Org',gitOrg,'Repo',gitRepo])
+    results.append("### Github Organization")
     if(gitOrg):
-      test1 = inqUrl('https://api.github.com/users/'+gitOrg)
-      print(test1)    # check for 'type': 'Organization', 'user_view_type': 'public'
-      test2 = inqUrl('https://api.github.com/orgs/'+gitOrg+'/members')
-      print(test2)
-      test3 = inqUrl('https://api.github.com/users/KMicha/orgs')
-      print(test3) 
-    else:
-      print('Running locally?') 
-    htmlReq = requests.get('https://github.com/'+gitOrg)
-    if(htmlReq.text):
-      print(htmlReq.text)
-    else:
-      print('people list failed')
+      orgData = inqUrl('https://api.github.com/users/'+gitOrg)
+      print(orgData)    # check for 'type': 'Organization', 'user_view_type': 'public'
+      if(not 'Organization'==orgData['type']):
+        results.append("Github Organization missing:")   
 
+        results.append("1. Please")
+        results.append("2. Logt")
+        results.append("3. Assifgn") 
+      else:
+        results.append("Github Organization exists") 
+        #orgMembers = inqUrl('https://api.github.com/orgs/'+gitOrg+'/members')
+        #print(orgMembers)
+        orgAssigned = False
+        myOrgs = inqUrl('https://api.github.com/users/KMicha/orgs')
+        print(myOrgs) 
+        for org in myOrgs:
+          if(org['id']==orgData['id']):
+            orgAssigned = True
+        if(orgAssigned):
+          results.append("Github Organization assigned")   
+        else:
+          results.append("Github Organization not assigned (or not public):")
+          results.append("1. Please")
+          results.append("2. Logt")
+          results.append("3. Assifgn")      
+    else:
+      print('No check possible: maybe running locally?') 
 
-checkGithubOrganization()
+results=[]
+checkGithubOrganization(results)
+print(results)
 
 
