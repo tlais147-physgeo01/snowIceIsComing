266de5e39c4df566d3f3fa691dbfda5fe00c84d7
KMicha <michael.kahle@yahoo.de>
2025-10-16 13:32:53 +0200

    Update check_api.py

--- check_api.py
+++ check_api.py
@@ -294,6 +294,7 @@ def inqRapidMicroTranslate3(results=[]):
             results.append(":no_entry: **Not** subscribed to Microsoft-Translator-3")
             addSubscribeMessageToResults(results, "Microsoft-Translator-3", "https://rapidapi.com/apiship-apiship-default/api/microsoft-translator-text-api3")
             return False
+        print(jsonData)     
         if (len(jsonData)>0):
           results.append(":white_check_mark: Microsoft-Translator-3 status fine")
           if ('translations' in jsonData[0]):
